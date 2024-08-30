ea24bfd306e34771b6ba3b79119297db5d8c4711
= <=>
2024-08-30 22:41:14 +0530

    added a toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <>
+    <Toaster richColors/>
     <App />
-  </StrictMode>,
-)
+  </>
+);
