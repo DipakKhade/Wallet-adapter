756ba99b6e63b57ed6685c9880762f5e0e11fdfd
= <=>
2024-08-30 22:43:18 +0530

    minor chnage

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -3,7 +3,7 @@ export default function Appbar(){
 
     return<>
     <div className="border border-slate-400 h-16">
-
+    <div className="pt-3 text-xl pl-12">Dipak Khade</div>
     </div>
     </>
 }
